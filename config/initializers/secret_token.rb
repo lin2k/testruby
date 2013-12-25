5dd9758b93e34bc56a8a9ecadf6c31c5a10d3555
lin2k <1153432481@qq.com>
2013-12-25 20:25:43 +0800

    fuck

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firsta::Application.config.secret_key_base = 'ec410c5ebe4a90bbe4582dd04680328e0173b177376b408d75b39db7f1db1eccd9578471b334376f4b1abd466a8778c6b805c10d05c0295da49c4621760a016e'
